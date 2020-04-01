21b0021ac9922e21722f3ecefc3e266ff6427955
ndo@ndo3 <yo@ni.co.de>
2020-04-01 15:34:53 +0200

    add: direct to dynamo upload function

--- functions/getData.js
+++ functions/getData.js
@@ -1,9 +1,11 @@
 exports.handler = (event, context, callback) => {
-  const fs = require('fs')
-  const exec = require('child_process').exec
   const fetch = require('isomorphic-unfetch')
   const AWS = require('aws-sdk')
-  const s3 = new AWS.S3()
+  const docClient = new AWS.DynamoDB.DocumentClient()
+  const uuidv4 = require('uuid/v4')
+  const { getS3object, putS3object } = require('./utils/s3')
+
+  const ddbTable = process.env.DDBtable
 
   const today = new Date()
   const year = today.getFullYear()
@@ -11,6 +13,62 @@ exports.handler = (event, context, callback) => {
   const day = `${today.getDate()}`.padStart(2, 0)
   const stringDate = [year, month, day].join('-')
   const fileDate = [year, month, day].join('')
+
+  const uploadJSONtoDynamoDB = async (data) => {
+    // Separate into batches for upload
+    const batches = []
+    const BATCH_SIZE = 25
+
+    while (data.length > 0) {
+      batches.push(data.splice(0, BATCH_SIZE))
+    }
+
+    console.log('Batches: ', batches.length)
+
+    let batchCount = 0
+
+    // Save each batch
+    await Promise.all(
+      batches.map(async (itemData) => {
+        // Set up the params object for the DDB call
+        const params = {
+          RequestItems: {}
+        }
+        params.RequestItems[ddbTable] = []
+
+        itemData.forEach(item => {
+          for (const key of Object.keys(item)) {
+            // An AttributeValue may not contain an empty string
+            if (item[key] === '') { delete item[key] }
+          }
+
+          // Build params
+          params.RequestItems[ddbTable].push({
+            PutRequest: {
+              Item: {
+                ID: uuidv4(),
+                ...item
+              }
+            }
+          })
+        })
+
+        // Push to DynamoDB in batches
+        try {
+          batchCount++
+          console.log('Trying batch: ', batchCount)
+          const result = await docClient.batchWrite(params).promise()
+          console.log('Success: ', result)
+          if (batchCount === batches.length) {
+            return { status: 200, msg: `${batches.length} ${batchCount} written` }
+          }
+        } catch (err) {
+          console.error('Error: ', err)
+          return { status: 500, msg: err }
+        }
+      })
+    )
+  }
 
   const response = body => {
     return {
@@ -29,43 +87,24 @@ exports.handler = (event, context, callback) => {
       data.forEach(entry => {
         entry.date = stringDate
       })
-      const params = {
-        Bucket: process.env.BUCKET_NAME,
-        Key: `data-json-${fileDate}.json`,
-        Body: data
+      const result = uploadJSONtoDynamoDB(data)
+      if (result.status === 200) {
+        return context.succeed(response({ status: 200, msg: result.msg }))
+      } else {
+        return context.fail(response({ status: 500, msg: result.msg }))
       }
-      // fs.writeFileSync(`/tmp/${stringDate}/data-aws-${fileDate}.json`, JSON.stringify(data))
-      s3.putObject(params, (err, data) => {
-        if (err) {
-          console.error(err)
-          return context.fail(response({ status: 500, msg: err }))
-        } else {
-          return context.succeed(response({ status: 200, msg: data }))
-        }
-      })
-
-      // exec(`aws s3 cp /tmp/${stringDate}/data-aws-${fileDate}.json s3://corona-api-json-content`, (err, stdout, stderr) => {
+      // const params = {
+      //   Bucket: process.env.BUCKET_NAME,
+      //   Key: `data-json-${fileDate}.json`,
+      //   Body: JSON.stringify(data)
+      // }
+      // s3.putObject(params, (err, data) => {
       //   if (err) {
-      //     return context.fail(response({ status: 'Error', msg: err || stderr }))
+      //     console.error(err)
+      //     return context.fail(response({ status: 500, msg: err }))
       //   } else {
-      //     return context.succeed(response({ status: 'Success', msg: stdout }))
+      //     return context.succeed(response({ status: 200, msg: data }))
       //   }
       // })
     })
-
-  // fs.readFile(`/tmp/${stringDate}/data.json`, 'utf8', (err, data) => {
-  //   if (err) throw err
-  //   const fileContent = JSON.parse(data)
-  //   fileContent.forEach(entry => {
-  //     entry.date = stringDate
-  //   })
-  //   fs.writeFileSync(`/tmp/${stringDate}/data-aws-${fileDate}.json`, JSON.stringify(fileContent))
-  //   exec(`aws s3 cp /tmp/${stringDate}/data-aws-${fileDate}.json s3://corona-api-json-content`, (err, stdout, stderr) => {
-  //     if (err) {
-  //       return context.fail(response({ status: 'Error', msg: err || stderr }))
-  //     } else {
-  //       return context.succeed(response({ status: 'Success', msg: stdout }))
-  //     }
-  //   })
-  // })
 }
---
+++ functions/utils/s3.js
@@ -0,0 +1,52 @@
+const AWS = require('aws-sdk')
+AWS.config.region = (process.env.AWS_REGION || 'us-east-1')
+const s3 = new AWS.S3()
+
+// Returns object from S3
+
+// let params = {
+//   Bucket: record.s3.bucket.name,
+//   Key: record.s3.object.key
+// }
+
+async function getS3object (params) {
+  return new Promise((resolve, reject) => {
+    s3.getObject(params, function (err, data) {
+      if (err) {
+        console.error('getS3object error: ', err, err.stack) // an error occurred
+        reject(err)
+      }
+      resolve(data)
+    })
+  })
+}
+
+// Puts object to S3 //
+
+// e.g. params = {
+//   Bucket: record.s3.bucket.name,
+//   Key: record.s3.object.key
+//   Body: data,
+//   ContentType: res.headers['content-type'],
+//   CacheControl: 'max-age=31536000',
+//   ACL: 'public-read',
+//   StorageClass: 'STANDARD'
+// }
+
+async function putS3object (params) {
+  console.log('putS3object params: ', params)
+  return new Promise((resolve, reject) => {
+    s3.putObject(params, function (err, data) {
+      if (err) {
+        console.log('putS3object error: ', err, err.stack) // an error occurred
+        reject(err)
+      }
+      resolve(data)
+    })
+  })
+}
+
+module.exports = {
+  getS3object,
+  putS3object
+}
