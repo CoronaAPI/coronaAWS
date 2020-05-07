b2d16d0f673ea45d2a358fdf837b39c854894f6c
ndo@ndo3 <yo@ndo.dev>
2020-05-07 19:54:29 +0200

    update: ruby dep.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const slsw = require('serverless-webpack');
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const slsw = require('serverless-webpack')
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -15,16 +15,16 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              presets: [['env', { targets: { node: '6.10' } }]],
-            },
-          },
-        ],
-      },
-    ],
+              presets: [['env', { targets: { node: '6.10' } }]]
+            }
+          }
+        ]
+      }
+    ]
   },
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
-};
+    filename: '[name].js'
+  }
+}
