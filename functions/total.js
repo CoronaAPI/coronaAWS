2bc1195b9c7247c6e1c9bcc0154ff779c394d595
Martin Lux <martin.lux@aoe.com>
2020-05-19 18:12:02 +0200

    Update util function

--- functions/total.js
+++ functions/total.js
@@ -1,23 +1,5 @@
 const { reduceData } = require('./utils/functions')
-
-async function getDynamoData () {
-  const AWS = require('aws-sdk')
-  const dynamo = new AWS.DynamoDB.DocumentClient()
-
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = `${today.getMonth() + 1}`.padStart(2, 0)
-  const day = `${today.getDate()}`.padStart(2, 0)
-  const TableData = await dynamo
-    .scan({
-      TableName: process.env.DDBtable
-    })
-    .promise()
-  const body = TableData.Items.filter(
-    (item) => item.date === `${year}-${month}-${day}`
-  )
-  return body
-}
+import { getDynamoData } from './utils/database'
 
 exports.handler = function (event, context, callback) {
   const redis = require('./utils/redis')()
--- functions/utils/database.js
+++ functions/utils/database.js
@@ -1,4 +1,4 @@
-export async function getDynamoData () {
+export async function getDynamoData() {
   const AWS = require('aws-sdk')
   const dynamo = new AWS.DynamoDB.DocumentClient()
 
@@ -6,13 +6,30 @@ export async function getDynamoData () {
   const year = today.getFullYear()
   const month = `${today.getMonth() + 1}`.padStart(2, 0)
   const day = `${today.getDate()}`.padStart(2, 0)
-  const TableData = await dynamo
-    .scan({
-      TableName: process.env.DDBtable
-    })
-    .promise()
-  const body = TableData.Items.filter(
-    (item) => item.date === `${year}-${month}-${day}`
-  )
-  return body
+
+  // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.04.html
+  var params = {
+    TableName: 'corona1',
+    IndexName: 'date-index',
+    ExpressionAttributeNames: {
+      '#d': 'date'
+    },
+    ExpressionAttributeValues: {
+      ':d': `${year}-${month}-${day}`
+    },
+    KeyConditionExpression: '#d = :d'
+  }
+
+  const returnData = await dynamo.query(params, function (err, data) {
+    if (err) {
+      console.error('Unable to query. Error:', JSON.stringify(err, null, 2))
+    } else {
+      console.log('Query succeeded.')
+      // data.Items.forEach(function (item) {
+      //   console.log(item)
+      // })
+      return data
+    }
+    return returnData
+  })
 }
